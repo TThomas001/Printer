422865235446b92ca8fb34d18018c0d0df51958b
ThomasC_TT <thomas-chen@tamu.edu>
2021-06-28 13:19:29 -0500

    add fan control feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
+#include <Arduino.h>
 #include<SPI.h>
 #include <ThingerESP32.h>
+#include <FanController.h>
+
+
+/*thinger.io setting*/
 #define DEBUG
 #define USERNAME "Thomas_tt"
 #define DEVICE_ID "ESP32"
@@ -7,27 +12,36 @@
 #define SSID "MyAltice e37ddd"
 #define SSID_PASSWORD "52-emerald-4063"
 
+/*DHT sensor setting*/
 #include "DHT.h"
 #define DHTTYPE DHT21
 #define DHTPIN 4   
 
+/*Fan control setting*/
+#define SENSOR_PIN 12
+#define SENSOR_THRESHOLD 1000 //the threshold will defind as time interval.(Large scale led to better accuracy)
+#define PWM_PIN 5
+#define min(a,b) ((a)<(b)?(a):(b))
+#define max(a,b) ((a)>(b)?(a):(b))
+
 ThingerESP32 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
 DHT dht(DHTPIN, DHTTYPE);
-
-int led = 2;       // GPIO02 - D4 
+FanController fan(SENSOR_PIN, SENSOR_THRESHOLD, PWM_PIN);
 
 void setup() {
   Serial.begin(115200);
   dht.begin();
+  fan.begin();
  // Initialization of the WiFi connection with THINGER.IO
-   thing.add_wifi(SSID, SSID_PASSWORD);
+  thing.add_wifi(SSID, SSID_PASSWORD);
 
- // Initialization of the LED
-   pinMode(led, OUTPUT);
+ thing["Fan"] << [](pson& in){
+   int Duty_cycle = in["Speed"];
+   byte target = max(min(Duty_cycle, 100), 0);
+   fan.setDutyCycle(target);
+   };
 
- // Resource for changing LED status from THINGER.IO
- /*thing["LED"] << [](pson& in){
-    if(in.is_empty()){
+   /* if(in.is_empty()){
       in = (bool) digitalRead(led);
       }
     else{
