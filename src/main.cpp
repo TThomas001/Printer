c59f2a61174dc74eaadc2b8b3c6248ac8cf345b6
ThomasC_TT <thomas-chen@tamu.edu>
2022-02-13 10:08:16 -0600

    modify

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,13 @@
 /*thinger.io setting*/
 #define USERNAME "Thomas_tt"
 #define DEVICE_ID "ESP32"
-#define DEVICE_CREDENTIAL "fa25y0%m@yzT1u78"
-#define SSID "MyAltice e37ddd"
-#define SSID_PASSWORD "52-emerald-4063"
-/*#define DEVICE_CREDENTIAL "C%GRMLy7R2!-!zw7"
-#define SSID "LAPTOP-IUIICA2V 4647"
-#define SSID_PASSWORD "G/7g5806"*/
-#define _DEBUG_
+/*#define DEVICE_CREDENTIAL "fa25y0%m@yzT1u78"
+#define SSID "Home"
+#define SSID_PASSWORD "Parkwest3622tamu!"
+*/
+#define DEVICE_CREDENTIAL "C%GRMLy7R2!-!zw7"
+#define SSID "LAPTOP-IUIICA2V4647"
+#define SSID_PASSWORD "G/7g5806"
 
 // the rest of your sketch goes here
 
@@ -22,18 +22,19 @@
 #define DHTPIN 4   
 
 /*Fan control setting*/
-#define SENSOR_PIN 26
+#define SENSOR_PIN 25
 #define SENSOR_THRESHOLD 1000 //the threshold will defind as time interval.(Large scale led to better accuracy)
-#define PWM_PIN 5
+#define PWM_PIN 33
 
 #define min(a,b) ((a)<(b)?(a):(b))
 #define max(a,b) ((a)>(b)?(a):(b))
 int Duty_cycle = 0;
+
 /*ADXL335 acceleromenter*/
 const int xInput = 32;
-const int yInput = 33;
+const int yInput = 5;
 const int zInput = 27;
-const int sampleSize = 1000;
+const int sampleSize = 100000;
 int RawMin = 0;
 int RawMax = 1023;
 
@@ -50,6 +51,7 @@ void setup() {
  // Initialization of the WiFi connection with THINGER.IO
   thing.add_wifi(SSID, SSID_PASSWORD);
 
+  pinMode(SENSOR_PIN, INPUT_PULLUP);// set the resister becuase the fan voltage reference outside source.
 
 /*Fan control API */
  thing["Fan"] = [](pson& in, pson& out){
@@ -59,11 +61,11 @@ void setup() {
     else{
         Duty_cycle = in["speed"];
     }
-  pinMode(SENSOR_PIN, INPUT_PULLUP);// set the resister becuase the fan voltage reference outside source.
 
    byte target = max(min(Duty_cycle, 100), 0);
    fan.setDutyCycle(target);
-   out["RPM"]=fan.getSpeed();
+   unsigned int rpms=fan.getSpeed();
+   out["RPM"]=rpms;
  };
 /*DHT API */
 
